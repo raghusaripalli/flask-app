093cd885221d49cd1b93dff46aea45fb47ed758b
raghusaripalli <rvs1604@gmail.com>
2019-02-02 21:37:16 +0530

    Added Articles Page

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+Articles = Articles()
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/articles")
+def articles():
+    return render_template("articles.html", articles=Articles)
+
+
+@app.route("/article/<string:aid>/")
+def article(aid):
+    return render_template("article.html", id=aid)
 
 
 @app.route("/about")
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolor, doloribus! Adipisci dolorem ex facilis ipsam odit quas suscipit vel veniam? A dolorum id in laudantium nam officia, soluta voluptas!',
+            'author': 'Dev Raghu',
+            'create_date': '02-02-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Twos',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolor, doloribus! Adipisci dolorem ex facilis ipsam odit quas suscipit vel veniam? A dolorum id in laudantium nam officia, soluta voluptas!',
+            'author': 'Tester Veer',
+            'create_date': '02-02-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolor, doloribus! Adipisci dolorem ex facilis ipsam odit quas suscipit vel veniam? A dolorum id in laudantium nam officia, soluta voluptas!',
+            'author': 'Dev Raghu',
+            'create_date': '02-02-2019'
+        }
+    ]
+    return articles
